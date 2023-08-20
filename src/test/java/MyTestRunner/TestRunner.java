10ef072a604d08421c942091ac395072583aaac2
Swaroop <>
2023-08-20 11:43:27 +0530

    Generated Report for test case

--- src/test/java/MyTestRunner/TestRunner.java
+++ src/test/java/MyTestRunner/TestRunner.java
@@ -10,8 +10,12 @@ import io.cucumber.junit.Cucumber;
 //Below Class use to add locations of feature and step Defination folder 
 //Because TestRunner la mahit nahiye ya files kuth ahet
 @CucumberOptions(
-			features = "Features",
-			glue = "StepDefination"	
+			features = "Features", 		//Directory of features file
+			glue = "StepDefination",	    //Directory or package name where the stepdefinition is present
+			//dryRun = true 				//ByDefault dryRun value is true and use to check the mapping is proper between feature and step def. file
+			plugin={"pretty","html:Output/report.html","json:Output/report.json","junit:Output/report.xml"},	//There are three reports we can get HTML,XML and JSON
+			monochrome = true				//Shows report properly..ie. which case/scenario is passed and failed
+						
 		)
 
 
--- src/test/java/StepDefination/MyAccountLoginStepDefination.java
+++ src/test/java/StepDefination/MyAccountLoginStepDefination.java
@@ -2,6 +2,7 @@ package StepDefination;
 
 import org.openqa.selenium.By;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
